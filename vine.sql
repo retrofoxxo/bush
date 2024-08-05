1e097d43315aeaa534386bab228ef00ad69a678f
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2024-08-05 15:58:54 +0200

    Oop forgot the database

---
+++ vine.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 05 aug 2024 om 15:56
+-- Serverversie: 10.4.32-MariaDB
+-- PHP-versie: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `sid` text NOT NULL,
+  `color` text DEFAULT '0x33ccbf',
+  `username` text NOT NULL,
+  `phone` text DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `followingc` int(11) NOT NULL DEFAULT 0,
+  `followersc` int(11) NOT NULL DEFAULT 0,
+  `verified` int(11) NOT NULL DEFAULT 0,
+  `description` text DEFAULT NULL,
+  `picture` text DEFAULT NULL,
+  `likec` int(11) NOT NULL DEFAULT 0,
+  `postc` int(11) NOT NULL DEFAULT 0,
+  `vphone` int(11) NOT NULL DEFAULT 0,
+  `vemail` int(11) NOT NULL DEFAULT 0,
+  `following` int(11) NOT NULL DEFAULT 0,
+  `location` text DEFAULT NULL,
+  `twitterid` text DEFAULT NULL,
+  `facebookid` text DEFAULT NULL,
+  `twittercon` int(11) NOT NULL DEFAULT 0,
+  `facebookcon` int(11) NOT NULL DEFAULT 0,
+  `suspended` int(11) NOT NULL DEFAULT 0,
+  `banned` int(11) NOT NULL DEFAULT 0,
+  `loopc` int(11) NOT NULL DEFAULT 0,
+  `passwordHash` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `receipient` int(11) NOT NULL DEFAULT 0,
+  `receipientAll` text NOT NULL DEFAULT 'n',
+  `message` text NOT NULL DEFAULT 'No Message content.',
+  `created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `postc` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `vines`
+--
+
+CREATE TABLE `vines` (
+  `id` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `commentc` int(11) NOT NULL DEFAULT 0,
+  `likec` int(11) NOT NULL DEFAULT 0,
+  `revinec` int(11) NOT NULL DEFAULT 0,
+  `loopc` int(11) NOT NULL DEFAULT 0,
+  `uploaderId` text NOT NULL,
+  `thumbnailUrl` text NOT NULL,
+  `videodef` text NOT NULL,
+  `videoh264` text NOT NULL,
+  `videor2` text NOT NULL,
+  `loopvelo` text NOT NULL DEFAULT '0.0',
+  `loopfire` int(11) NOT NULL DEFAULT 0,
+  `created` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `vines`
+--
+ALTER TABLE `vines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `vines`
+--
+ALTER TABLE `vines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
